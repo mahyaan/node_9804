06679ad1ce57a33665c4249e1c491f9938f50f2e
unknown <mahya@gmail.com>
2020-02-28 12:38:40 +0330

    add all file

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/getname/:name?/:family?', function(req, res, next) {
+  const name = req.params.name || 'Express';
+  const family = req.params.family || 'nzh';
+
+  res.render('index', { titlllle: name+family});
+});
+
+module.exports = router;
+
+
+// :name =>343434 ////ali
