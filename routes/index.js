932f6f609be9fa36958238dcc0d594a060af0975
unknown <mahya@gmail.com>
2020-02-28 13:33:17 +0330

    ADD ROUES

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/getname/:name?/:family?', function(req, res, next) {
-  const name = req.params.name || 'Express';
-  const family = req.params.family || 'nzh';
-
-  res.render('index', { titlllle: name+family});
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
-
-
-// :name =>343434 ////ali
-///89789
